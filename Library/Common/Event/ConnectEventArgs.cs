a07d3a69df4d104857ea786ee41f15669c0f0fb8
Mili <mili@mili.cyou>
2023-04-01 21:13:28 +0800

    一个没有测试就提交的BUG

--- Library/Common/Event/ConnectEventArgs.cs
+++ Library/Common/Event/ConnectEventArgs.cs
@@ -2,15 +2,15 @@
 {
     public class ConnectEventArgs : GeneralEventArgs
     {
-        public string ServerIP { get; set; } = "";
-        public string ServerPort { get; set; } = "";
+        public string ServerIP { get; set; } = "127.0.0.1";
+        public int ServerPort { get; set; } = 22222;
 
         public ConnectEventArgs(params object[]? objs)
         {
             if (objs != null)
             {
                 if (objs.Length > 0) ServerIP = (string)objs[0];
-                if (objs.Length > 1) ServerPort = (string)objs[1];
+                if (objs.Length > 1) ServerPort = (int)objs[1];
             }
         }
     }
