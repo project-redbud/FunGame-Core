7637b1e0ba85ab7b277924117183a987aa619d30
Mili <mili@mili.cyou>
2023-05-24 19:59:04 +0800

    添加ForgetPassword相关表和查询

--- Library/SQLScript/Common/Common.cs
+++ Library/SQLScript/Common/Common.cs
@@ -39,33 +39,4 @@ namespace Milimoe.FunGame.Core.Library.SQLScript.Common
             return $"{Constant.Command_Insert} {Constant.Command_Into} {TableName} ({Column_ServerName}, {Column_ServerKey}, {Column_LoginTime}) {Constant.Command_Values} ('{ServerName}', '{ServerKey}', '{DateTime.Now}')";
         }
     }
-
-    public class RegVerifyCodes
-    {
-        public const string TableName = "RegVerifyCodes";
-        public const string Column_Username = "Username";
-        public const string Column_Email = "Email";
-        public const string Column_RegVerifyCode = "RegVerifyCode";
-        public const string Column_RegTime = "RegTime";
-
-        public static string Insert_RegVerifyCodes(string Username, string Email, string RegVerifyCodes)
-        {
-            return $"{Constant.Command_Insert} {Constant.Command_Into} {TableName} ({Column_Username}, {Column_Email}, {Column_RegVerifyCode}, {Column_RegTime}) {Constant.Command_Values} ('{Username}', '{Email}', '{RegVerifyCodes}', '{DateTime.Now}')";
-        }
-
-        public static string Select_RegVerifyCode(string Username, string Email, string RegVerifyCode)
-        {
-            return $"{Constant.Command_Select} {Constant.Command_All} {Constant.Command_From} {TableName} {Constant.Command_Where} {Column_Username} = '{Username}' and {Column_Email} = '{Email}' and {Column_RegVerifyCode} = '{RegVerifyCode}'";
-        }
-        
-        public static string Select_HasSentRegVerifyCode(string Username, string Email)
-        {
-            return $"{Constant.Command_Select} {Constant.Command_All} {Constant.Command_From} {TableName} {Constant.Command_Where} {Column_Username} = '{Username}' and {Column_Email} = '{Email}'";
-        }
-
-        public static string Delete_RegVerifyCode(string Username, string Email)
-        {
-            return $"{Constant.Command_Delete} {Constant.Command_From} {TableName} {Constant.Command_Where} {Column_Username} = '{Username}' and {Column_Email} = '{Email}'";
-        }
-    }
 }
