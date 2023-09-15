8c27660adb77901227002f7d87d25aaf50a32b94
milimoe <mili@wrss.org>
2023-09-15 23:08:31 +0800

    开发插件接口和基类

---
+++ Interface/Base/IPlugin.cs
@@ -0,0 +1,12 @@
+﻿namespace Milimoe.FunGame.Core.Interface
+{
+    public interface IPlugin
+    {
+        public string Name { get; }
+        public string Description { get; }
+        public string Version { get; }
+        public string Author { get; }
+
+        public void Load();
+    }
+}
---
+++ Library/Common/Plugin/Example.cs
@@ -0,0 +1,33 @@
+﻿using Milimoe.FunGame.Core.Interface;
+using Milimoe.FunGame.Core.Library.Common.Event;
+using Milimoe.FunGame.Core.Library.Constant;
+
+namespace Milimoe.FunGame.Core.Library.Common.Plugin
+{
+    /// <summary>
+    /// 必须继承基类：<see cref="BasePlugin"/><para/>
+    /// 继承事件接口并实现其方法来使插件生效。例如继承：<seealso cref="ILoginEvent"/>
+    /// </summary>
+    public class Example : BasePlugin, ILoginEvent
+    {
+        public EventResult AfterLoginEvent(object sender, LoginEventArgs e)
+        {
+            return EventResult.Success;
+        }
+
+        public EventResult BeforeLoginEvent(object sender, LoginEventArgs e)
+        {
+            return EventResult.Success;
+        }
+
+        public EventResult FailedLoginEvent(object sender, LoginEventArgs e)
+        {
+            return EventResult.Success;
+        }
+
+        public EventResult SucceedLoginEvent(object sender, LoginEventArgs e)
+        {
+            return EventResult.Success;
+        }
+    }
+}
