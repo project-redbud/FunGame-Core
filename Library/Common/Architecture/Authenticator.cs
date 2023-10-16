6876430f7a4811b9c849c838b5f9d4befb8cf194
milimoe <mili@wrss.org>
2023-10-16 22:58:00 +0800

    Add more authenticators

--- Library/Common/Architecture/Authenticator.cs
+++ Library/Common/Architecture/Authenticator.cs
@@ -1,5 +1,7 @@
 ﻿using System.Data;
+using System.Data.Common;
 using Milimoe.FunGame.Core.Api.Transmittal;
+using Milimoe.FunGame.Core.Library.SQLScript.Entity;
 
 namespace Milimoe.FunGame.Core.Library.Common.Architecture
 {
@@ -12,6 +14,21 @@ namespace Milimoe.FunGame.Core.Library.Common.Architecture
             this.SQLHelper = SQLHelper;
         }
 
+        public bool Authenticate(string script)
+        {
+            SQLHelper.ExecuteDataSet(script);
+            if (SQLHelper.Success)
+            {
+                DataSet ds = SQLHelper.DataSet;
+                if (ds.Tables.Count > 0 &&
+                    ds.Tables[0].Rows.Count > 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
         public bool Authenticate<T>(string script, string column, T keyword)
         {
             SQLHelper.ExecuteDataSet(script);
@@ -22,6 +39,24 @@ namespace Milimoe.FunGame.Core.Library.Common.Architecture
                     ds.Tables[0].Columns.Contains(column) &&
                     ds.Tables[0].Rows.Count > 0 &&
                     ds.Tables[0].AsEnumerable().Where(row => row.Field<T>(column)!.Equals(keyword)).Any())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        
+        public bool Authenticate(string script, string username, string password)
+        {
+            SQLHelper.ExecuteDataSet(script);
+            if (SQLHelper.Success)
+            {
+                DataSet ds = SQLHelper.DataSet;
+                if (ds.Tables.Count > 0 &&
+                    ds.Tables[0].Columns.Contains(UserQuery.Column_Username) &&
+                    ds.Tables[0].Columns.Contains(UserQuery.Column_Password) &&
+                    ds.Tables[0].Rows.Count > 0 &&
+                    ds.Tables[0].AsEnumerable().Where(row => (string)row[UserQuery.Column_Username] == username && (string)row[UserQuery.Column_Password] == password).Any())
                 {
                     return true;
                 }
