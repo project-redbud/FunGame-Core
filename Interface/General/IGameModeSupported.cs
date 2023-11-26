54a435aadfd6b170ea27a5caaee168ca54e17edb
milimoe <mili@wrss.org>
2023-11-26 19:19:58 +0800

    添加IGameModeSupported接口

---
+++ Interface/General/IGameModeSupported.cs
@@ -0,0 +1,11 @@
+﻿namespace Milimoe.FunGame.Core.Interface
+{
+    /// <summary>
+    /// 服务端和客户端都应该实现这个接口，用于初始化支持的Mod列表
+    /// </summary>
+    public interface IGameModeSupported
+    {
+        public string[] GameModeList { get; }
+        public string[] GameMapList { get; }
+    }
+}
