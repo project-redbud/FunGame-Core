a403ca63ff4acec2f4725d70db54221bee0ea7b3
milimoe <mili@wrss.org>
2023-12-08 01:06:00 +0800

    添加马甲方法，隐藏委托

--- Controller/AddonController.cs
+++ Controller/AddonController.cs
@@ -11,18 +11,39 @@ namespace Milimoe.FunGame.Core.Controller
         /// <summary>
         /// 输出系统消息
         /// </summary>
-        public Action<string> WriteLine { get; set; } = new(msg => Console.Write("\r" + msg + "\n\r> "));
+        private Action<string> MaskMethod_WriteLine { get; set; } = new(msg => Console.Write("\r" + msg + "\n\r> "));
 
         /// <summary>
         /// 基于本地已连接的Socket创建新的数据请求
         /// </summary>
-        public Func<DataRequestType, DataRequest> NewDataRequest { get; set; }
+        private Func<DataRequestType, DataRequest> MaskMethod_NewDataRequest { get; set; }
 
         /// <summary>
         /// 基于本地已连接的Socket创建长时间运行的数据请求
         /// </summary>
-        public Func<DataRequestType, DataRequest> NewLongRunningDataRequest { get; set; }
+        private Func<DataRequestType, DataRequest> MaskMethod_NewLongRunningDataRequest { get; set; }
 
+        /// <summary>
+        /// 输出系统消息
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <returns></returns>
+        public void WriteLine(string msg) => MaskMethod_WriteLine(msg);
+
+        /// <summary>
+        /// 基于本地已连接的Socket创建新的数据请求
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public DataRequest NewDataRequest(DataRequestType type) => MaskMethod_NewDataRequest(type);
+
+        /// <summary>
+        /// 基于本地已连接的Socket创建长时间运行的数据请求
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public DataRequest NewLongRunningDataRequest(DataRequestType type) => MaskMethod_NewLongRunningDataRequest(type);
+        
         /// <summary>
         /// 新建一个AddonController
         /// </summary>
@@ -31,17 +52,11 @@ namespace Milimoe.FunGame.Core.Controller
         public AddonController(IAddon Addon, Delegate[] delegates)
         {
             this.Addon = Addon;
-            if (delegates.Length > 0) WriteLine = (Action<string>)delegates[0];
-            if (delegates.Length > 1) NewDataRequest = (Func<DataRequestType, DataRequest>)delegates[1];
-            if (delegates.Length > 2) NewLongRunningDataRequest = (Func<DataRequestType, DataRequest>)delegates[2];
-            if (NewDataRequest is null)
-            {
-                NewDataRequest = new(DefaultNewDataRequest);
-            }
-            if (NewLongRunningDataRequest is null)
-            {
-                NewLongRunningDataRequest = new(DefaultNewDataRequest);
-            }
+            if (delegates.Length > 0) MaskMethod_WriteLine = (Action<string>)delegates[0];
+            if (delegates.Length > 1) MaskMethod_NewDataRequest = (Func<DataRequestType, DataRequest>)delegates[1];
+            if (delegates.Length > 2) MaskMethod_NewLongRunningDataRequest = (Func<DataRequestType, DataRequest>)delegates[2];
+            MaskMethod_NewDataRequest ??= new(DefaultNewDataRequest);
+            MaskMethod_NewLongRunningDataRequest ??= new(DefaultNewDataRequest);
         }
 
         private DataRequest DefaultNewDataRequest(DataRequestType type)
