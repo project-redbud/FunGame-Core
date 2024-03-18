f597f6529a29806998515e6185e93ed8c459f2e4
milimoe <mili@wrss.org>
2024-03-18 20:44:21 +0800

    modify

--- Api/Utility/PluginConfig.cs
+++ Api/Utility/PluginConfig.cs
@@ -1,32 +1,30 @@
+using System.Text;
+using System.Text.Json;
+using Milimoe.FunGame.Core.Library.Constant;
+
 namespace Milimoe.FunGame.Core.Api.Utility
 {
     /// <summary>
     /// 简易的插件配置文件生成器<para/>
     /// 仅支持部分基本类型（<see cref="long"/>, <see cref="decimal"/>, <see cref="string"/>, <see cref="bool"/>）及其数组（<see cref="List{T}">List&lt;long&gt;, List&lt;decimal&gt;, List&lt;string&gt;, List&lt;bool&gt;</see>和<see cref="Array">long[], decimal[], string[], bool[]</see>）
-    /// <para/>文件会保存为：程序目录/configs/<see cref="PluginName"/>/<see cref="FileName"/>.conf
+    /// <para/>文件会保存为：程序目录/configs/<see cref="PluginName"/>/<see cref="FileName"/>.json
     /// </summary>
-    public class PluginConfig : Dictionary<string, object>
+    /// <remarks>
+    /// 新建一个配置文件，文件会保存为：程序目录/configs/<see cref="PluginName"/>/<see cref="FileName"/>.json
+    /// </remarks>
+    /// <param name="plugin_name"></param>
+    /// <param name="file_name"></param>
+    public class PluginConfig(string plugin_name, string file_name) : Dictionary<string, object>
     {
         /// <summary>
         /// 插件的名称
         /// </summary>
-        public string PluginName { get; set; } = "";
+        public string PluginName { get; set; } = plugin_name;
 
         /// <summary>
-        /// 配置文件的名称（后缀将是.conf）
+        /// 配置文件的名称（后缀将是.json）
         /// </summary>
-        public string FileName { get; set; } = "";
-
-        /// <summary>
-        /// 新建一个配置文件，文件会保存为：程序目录/configs/<see cref="PluginName"/>/<see cref="FileName"/>.conf
-        /// </summary>
-        /// <param name="plugin_name"></param>
-        /// <param name="file_name"></param>
-        public PluginConfig(string plugin_name, string file_name)
-        {
-            PluginName = plugin_name;
-            FileName = file_name;
-        }
+        public string FileName { get; set; } = file_name;
 
         /// <summary>
         /// 使用索引器给指定key赋值，不存在key会新增
@@ -35,10 +33,7 @@ namespace Milimoe.FunGame.Core.Api.Utility
         /// <returns></returns>
         public new object this[string key]
         {
-            set
-            {
-                Add(key, value);
-            }
+            set => Add(key, value);
         }
         
         /// <summary>
@@ -48,10 +43,13 @@ namespace Milimoe.FunGame.Core.Api.Utility
         /// <param name="key"></param>
         public void Parse<T>(string key)
         {
-            if (TryGetValue(key, out object value))
+            if (TryGetValue(key, out object? value) && value != null)
             {
-                T instance = JsonSerializer.Deserialize<T>(value.ToString());
-                base[key] = instance;
+                T? instance = NetworkUtility.JsonDeserialize<T>(value.ToString() ?? "");
+                if (instance != null)
+                {
+                    base[key] = instance;
+                }
             }
         }
         
@@ -61,11 +59,11 @@ namespace Milimoe.FunGame.Core.Api.Utility
         /// <typeparam name="T"></typeparam>
         /// <param name="key"></param>
         /// <returns></returns>
-        public T Get<T>(string key)
+        public T? Get<T>(string key)
         {
-            if (TryGetValue(key, out object value))
+            if (TryGetValue(key, out object? value) && value != null)
             {
-                return JsonSerializer.Deserialize<T>(value.ToString());
+                return NetworkUtility.JsonDeserialize<T>(value.ToString() ?? "");
             }
             return default;
         }
@@ -85,21 +83,21 @@ namespace Milimoe.FunGame.Core.Api.Utility
         }
 
         /// <summary>
-        /// 从配置文件中读取配置
-        /// 注意：所有保存时为数组的对象都会变成List对象
+        /// 从配置文件中读取配置。
+        /// 注意：所有保存时为数组的对象都会变成<see cref="List{T}"/>对象，并且不支持<see cref="object"/>类型
         /// </summary>
         public void LoadConfig()
         {
             string dpath = $@"{AppDomain.CurrentDomain.BaseDirectory}configs/{PluginName}";
-            string fpath = $@"{dpath}/{FileName}.conf";
+            string fpath = $@"{dpath}/{FileName}.json";
             if (!Directory.Exists(dpath))
             {
                 Directory.CreateDirectory(dpath);
             }
             if (File.Exists(fpath))
             {
-                string json = File.ReadAllText(fpath, Encoding.UTF8);
-                Dictionary<string, object> dict = JsonSerializer.Deserialize<Dictionary<string, object>>(json);
+                string json = File.ReadAllText(fpath, General.DefaultEncoding);
+                Dictionary<string, object> dict = NetworkUtility.JsonDeserialize<Dictionary<string, object>>(json) ?? [];
                 Clear();
                 foreach (string key in dict.Keys)
                 {
@@ -110,24 +108,20 @@ namespace Milimoe.FunGame.Core.Api.Utility
         }
 
         /// <summary>
-        /// 将配置保存到配置文件
+        /// 将配置保存到配置文件。调用此方法会覆盖原有的.json，请注意备份
         /// </summary>
         public void SaveConfig()
         {
-            string json = JsonSerializer.Serialize((Dictionary<string, object>)this, new JsonSerializerOptions()
-            {
-                WriteIndented = true
-            });
+            string json = NetworkUtility.JsonSerialize((Dictionary<string, object>)this);
             string dpath = $@"{AppDomain.CurrentDomain.BaseDirectory}configs/{PluginName}";
-            string fpath = $@"{dpath}/{FileName}.conf";
+            string fpath = $@"{dpath}/{FileName}.json";
             if (!Directory.Exists(dpath))
             {
                 Directory.CreateDirectory(dpath);
             }
-            StreamWriter writer = File.CreateText(fpath);
+            using StreamWriter writer = new(fpath, false, Encoding.Unicode);
             writer.WriteLine(json);
             writer.Flush();
-            writer.Close();
         }
 
         /// <summary>
@@ -153,7 +147,7 @@ namespace Milimoe.FunGame.Core.Api.Utility
                     }
                     break;
                 case JsonValueKind.String:
-                    base.Add(key, obj.GetString());
+                    base.Add(key, obj.GetString() ?? "");
                     break;
                 case JsonValueKind.True:
                 case JsonValueKind.False:
@@ -166,16 +160,16 @@ namespace Milimoe.FunGame.Core.Api.Utility
         }
 
         /// <summary>
-        /// Json数组反序列化的方法
+        /// Json数组反序列化的方法。不支持<see cref="object"/>数组。
         /// </summary>
         /// <param name="key"></param>
         /// <param name="obj"></param>
         private void AddValues(string key, JsonElement.ArrayEnumerator obj)
         {
-            List<long> longList = new();
-            List<decimal> decList = new();
-            List<string> strList = new();
-            List<bool> bolList = new();
+            List<long> longList = [];
+            List<decimal> decList = [];
+            List<string> strList = [];
+            List<bool> bolList = [];
             foreach (JsonElement array_e in obj)
             {
                 if (array_e.ValueKind == JsonValueKind.Number && array_e.TryGetInt64(out long longValue))
