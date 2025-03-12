f41361289c9b9a99ca331d7d0173ed94256df616
milimoe <mili@wrss.org>
2025-03-12 21:45:27 +0800

    删除加密方法的第二次哈希（中断性）

--- Api/Utility/StringExtension.cs
+++ Api/Utility/StringExtension.cs
@@ -7,14 +7,13 @@
     {
         /// <summary>
         /// 使用 HMAC-SHA512 算法对文本进行加密<para/>
-        /// 注意：此方法会先将 <paramref name="key" /> 转为小写并计算两次哈希。
         /// </summary>
         /// <param name="text">需要加密的文本</param>
         /// <param name="key">用于加密的秘钥</param>
         /// <returns>加密后的 HMAC-SHA512 哈希值</returns>
         public static string Encrypt(this string text, string key)
         {
-            return Encryption.HmacSha512(text, Encryption.HmacSha512(text, key.ToLower()));
+            return Encryption.HmacSha512(text, key.ToLower());
         }
     }
 }
