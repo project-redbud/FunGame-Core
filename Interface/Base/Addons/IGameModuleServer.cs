7561c2c7e21ac014b28d9e05ea7cc022354b6c31
milimoe <110188673+milimoe@users.noreply.github.com>
2025-03-14 19:35:54 +0800

    添加 GamingObject；优化整体模组服务器的底层设计 (#113)
    更新示例代码（添加超时示例）

--- Interface/Base/Addons/IGameModuleServer.cs
+++ Interface/Base/Addons/IGameModuleServer.cs
@@ -1,12 +1,12 @@
-﻿using Milimoe.FunGame.Core.Entity;
-using Milimoe.FunGame.Core.Interface.Base;
+﻿using Milimoe.FunGame.Core.Interface.Base;
+using Milimoe.FunGame.Core.Library.Common.Addon;
 using Milimoe.FunGame.Core.Library.Constant;
 
 namespace Milimoe.FunGame.Core.Interface.Addons
 {
     public interface IGameModuleServer : IAddon, IAddonController<IGameModuleServer>, IGameModuleDepend
     {
-        public bool StartServer(string GameModule, Room Room, List<User> Users, IServerModel RoomMasterServerModel, Dictionary<string, IServerModel> ServerModels, params object[] args);
+        public bool StartServer(GamingObject obj, params object[] args);
 
         public Task<Dictionary<string, object>> GamingMessageHandler(IServerModel model, GamingType type, Dictionary<string, object> data);
     }
---
+++ Library/Common/Addon/GamingObject.cs
@@ -0,0 +1,13 @@
+﻿using Milimoe.FunGame.Core.Entity;
+using Milimoe.FunGame.Core.Interface.Base;
+
+namespace Milimoe.FunGame.Core.Library.Common.Addon
+{
+    public readonly struct GamingObject(Room room, List<User> users, IServerModel roomMaster, Dictionary<string, IServerModel> serverModels)
+    {
+        public Room Room { get; } = room;
+        public List<User> Users { get; } = users;
+        public IServerModel RoomMaster { get; } = roomMaster;
+        public Dictionary<string, IServerModel> All { get; } = serverModels;
+    }
+}
