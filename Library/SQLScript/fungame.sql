884d1e2560e0dbf323b1e6ef2611b28698d5645d
milimoe <mili@wrss.org>
2025-03-25 23:46:55 +0800

    更新sqlite

--- Library/SQLScript/fungame.sql
+++ Library/SQLScript/fungame.sql
@@ -1,268 +1,268 @@
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for apitokens
+-- Table structure for ApiTokens
 -- ----------------------------
-DROP TABLE IF EXISTS `apitokens`;
-CREATE TABLE `apitokens` (
-  `TokenID` varchar(255) NOT NULL DEFAULT '',
-  `SecretKey` varchar(255) NOT NULL DEFAULT '',
-  `Reference1` varchar(255) NOT NULL DEFAULT '',
-  `Reference2` varchar(255) NOT NULL DEFAULT '',
-  PRIMARY KEY (`TokenID`)
+DROP TABLE IF EXISTS `ApiTokens`;
+CREATE TABLE `ApiTokens` (
+ `TokenID` varchar(255) NOT NULL DEFAULT '',
+ `SecretKey` varchar(255) NOT NULL DEFAULT '',
+ `Reference1` varchar(255) NOT NULL DEFAULT '',
+ `Reference2` varchar(255) NOT NULL DEFAULT '',
+ PRIMARY KEY (`TokenID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
--- Table structure for forgetverifycodes
+-- Table structure for ForgetVerifyCodes
 -- ----------------------------
-DROP TABLE IF EXISTS `forgetverifycodes`;
-CREATE TABLE `forgetverifycodes` (
-  `Username` varchar(255) NOT NULL DEFAULT '',
-  `Email` varchar(255) NOT NULL DEFAULT '',
-  `ForgetVerifyCode` varchar(255) NOT NULL DEFAULT '',
-  `SendTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+DROP TABLE IF EXISTS `ForgetVerifyCodes`;
+CREATE TABLE `ForgetVerifyCodes` (
+ `Username` varchar(255) NOT NULL DEFAULT '',
+ `Email` varchar(255) NOT NULL DEFAULT '',
+ `ForgetVerifyCode` varchar(255) NOT NULL DEFAULT '',
+ `SendTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
--- Table structure for gooditems
+-- Table structure for GoodItems
 -- ----------------------------
-DROP TABLE IF EXISTS `gooditems`;
-CREATE TABLE `gooditems` (
-  `Id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `GoodId` bigint(20) NOT NULL DEFAULT '0',
-  `ItemId` bigint(20) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`Id`)
+DROP TABLE IF EXISTS `GoodItems`;
+CREATE TABLE `GoodItems` (
+ `Id` bigint(20) NOT NULL AUTO_INCREMENT,
+ `GoodId` bigint(20) NOT NULL DEFAULT '0',
+ `ItemId` bigint(20) NOT NULL DEFAULT '0',
+ PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
--- Table structure for goodprices
+-- Table structure for GoodPrices
 -- ----------------------------
-DROP TABLE IF EXISTS `goodprices`;
-CREATE TABLE `goodprices` (
-  `Id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `GoodId` bigint(20) NOT NULL DEFAULT '0',
-  `Currency` varchar(255) NOT NULL DEFAULT '',
-  `Price` double(20,0) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`Id`)
+DROP TABLE IF EXISTS `GoodPrices`;
+CREATE TABLE `GoodPrices` (
+ `Id` bigint(20) NOT NULL AUTO_INCREMENT,
+ `GoodId` bigint(20) NOT NULL DEFAULT '0',
+ `Currency` varchar(255) NOT NULL DEFAULT '',
+ `Price` double(20,0) NOT NULL DEFAULT '0',
+ PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
--- Table structure for goods
+-- Table structure for Goods
 -- ----------------------------
-DROP TABLE IF EXISTS `goods`;
-CREATE TABLE `goods` (
-  `Id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `Name` varchar(255) NOT NULL DEFAULT '',
-  `Description` varchar(255) NOT NULL DEFAULT '',
-  `Stock` int(10) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`Id`)
+DROP TABLE IF EXISTS `Goods`;
+CREATE TABLE `Goods` (
+ `Id` bigint(20) NOT NULL AUTO_INCREMENT,
+ `Name` varchar(255) NOT NULL DEFAULT '',
+ `Description` varchar(255) NOT NULL DEFAULT '',
+ `Stock` int(10) NOT NULL DEFAULT '0',
+ PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
--- Table structure for inventories
+-- Table structure for Inventories
 -- ----------------------------
-DROP TABLE IF EXISTS `inventories`;
-CREATE TABLE `inventories` (
-  `UserId` bigint(20) NOT NULL DEFAULT '0',
-  `Name` varchar(255) NOT NULL DEFAULT '',
-  `Credits` decimal(20,0) NOT NULL DEFAULT '0',
-  `Materials` decimal(20,0) NOT NULL DEFAULT '0',
-  `MainCharacter` bigint(20) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`UserId`)
+DROP TABLE IF EXISTS `Inventories`;
+CREATE TABLE `Inventories` (
+ `UserId` bigint(20) NOT NULL DEFAULT '0',
+ `Name` varchar(255) NOT NULL DEFAULT '',
+ `Credits` decimal(20,0) NOT NULL DEFAULT '0',
+ `Materials` decimal(20,0) NOT NULL DEFAULT '0',
+ `MainCharacter` bigint(20) NOT NULL DEFAULT '0',
+ PRIMARY KEY (`UserId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
--- Table structure for marketitems
+-- Table structure for MarketItems
 -- ----------------------------
-DROP TABLE IF EXISTS `marketitems`;
-CREATE TABLE `marketitems` (
-  `Id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `ItemId` bigint(255) NOT NULL DEFAULT '0',
-  `UserId` bigint(255) NOT NULL DEFAULT '0',
-  `Price` double(20,0) NOT NULL DEFAULT '0',
-  `CreateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `FinishTime` datetime DEFAULT NULL,
-  `Status` int(10) NOT NULL DEFAULT '0',
-  `Buyer` bigint(20) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`Id`,`ItemId`,`UserId`)
+DROP TABLE IF EXISTS `MarketItems`;
+CREATE TABLE `MarketItems` (
+ `Id` bigint(20) NOT NULL AUTO_INCREMENT,
+ `ItemId` bigint(255) NOT NULL DEFAULT '0',
+ `UserId` bigint(255) NOT NULL DEFAULT '0',
+ `Price` double(20,0) NOT NULL DEFAULT '0',
+ `CreateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ `FinishTime` datetime DEFAULT NULL,
+ `Status` int(10) NOT NULL DEFAULT '0',
+ `Buyer` bigint(20) NOT NULL DEFAULT '0',
+ PRIMARY KEY (`Id`,`ItemId`,`UserId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
--- Table structure for offeritems
+-- Table structure for OfferItems
 -- ----------------------------
-DROP TABLE IF EXISTS `offeritems`;
-CREATE TABLE `offeritems` (
-  `Id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `OfferId` bigint(20) NOT NULL DEFAULT '0',
-  `UserId` bigint(20) NOT NULL DEFAULT '0',
-  `ItemId` bigint(20) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`Id`)
+DROP TABLE IF EXISTS `OfferItems`;
+CREATE TABLE `OfferItems` (
+ `Id` bigint(20) NOT NULL AUTO_INCREMENT,
+ `OfferId` bigint(20) NOT NULL DEFAULT '0',
+ `UserId` bigint(20) NOT NULL DEFAULT '0',
+ `ItemId` bigint(20) NOT NULL DEFAULT '0',
+ PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
--- Table structure for offers
+-- Table structure for Offers
 -- ----------------------------
-DROP TABLE IF EXISTS `offers`;
-CREATE TABLE `offers` (
-  `Id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `Offeror` bigint(20) NOT NULL DEFAULT '0',
-  `Offeree` bigint(20) NOT NULL DEFAULT '0',
-  `CreateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `FinishTime` datetime DEFAULT NULL,
-  `Status` int(10) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`Id`)
+DROP TABLE IF EXISTS `Offers`;
+CREATE TABLE `Offers` (
+ `Id` bigint(20) NOT NULL AUTO_INCREMENT,
+ `Offeror` bigint(20) NOT NULL DEFAULT '0',
+ `Offeree` bigint(20) NOT NULL DEFAULT '0',
+ `CreateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ `FinishTime` datetime DEFAULT NULL,
+ `Status` int(10) NOT NULL DEFAULT '0',
+ PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
--- Table structure for regverifycodes
+-- Table structure for RegVerifyCodes
 -- ----------------------------
-DROP TABLE IF EXISTS `regverifycodes`;
-CREATE TABLE `regverifycodes` (
-  `Username` varchar(255) NOT NULL DEFAULT '',
-  `Email` varchar(255) NOT NULL DEFAULT '',
-  `RegVerifyCode` varchar(255) NOT NULL DEFAULT '',
-  `RegTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+DROP TABLE IF EXISTS `RegVerifyCodes`;
+CREATE TABLE `RegVerifyCodes` (
+ `Username` varchar(255) NOT NULL DEFAULT '',
+ `Email` varchar(255) NOT NULL DEFAULT '',
+ `RegVerifyCode` varchar(255) NOT NULL DEFAULT '',
+ `RegTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
--- Table structure for rooms
+-- Table structure for Rooms
 -- ----------------------------
-DROP TABLE IF EXISTS `rooms`;
-CREATE TABLE `rooms` (
-  `Id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `Roomid` varchar(255) NOT NULL DEFAULT '-1',
-  `CreateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `RoomMaster` bigint(20) NOT NULL DEFAULT '0',
-  `RoomType` int(8) NOT NULL DEFAULT '0',
-  `GameModule` varchar(255) NOT NULL DEFAULT '',
-  `GameMap` varchar(255) NOT NULL DEFAULT '',
-  `RoomState` int(8) NOT NULL DEFAULT '0',
-  `IsRank` int(1) NOT NULL DEFAULT '0',
-  `HasPass` int(1) NOT NULL DEFAULT '0',
-  `Password` varchar(255) NOT NULL DEFAULT '',
-  `MaxUsers` int(8) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`Id`,`Roomid`)
+DROP TABLE IF EXISTS `Rooms`;
+CREATE TABLE `Rooms` (
+ `Id` bigint(20) NOT NULL AUTO_INCREMENT,
+ `Roomid` varchar(255) NOT NULL DEFAULT '-1',
+ `CreateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ `RoomMaster` bigint(20) NOT NULL DEFAULT '0',
+ `RoomType` int(8) NOT NULL DEFAULT '0',
+ `GameModule` varchar(255) NOT NULL DEFAULT '',
+ `GameMap` varchar(255) NOT NULL DEFAULT '',
+ `RoomState` int(8) NOT NULL DEFAULT '0',
+ `IsRank` int(1) NOT NULL DEFAULT '0',
+ `HasPass` int(1) NOT NULL DEFAULT '0',
+ `Password` varchar(255) NOT NULL DEFAULT '',
+ `MaxUsers` int(8) NOT NULL DEFAULT '0',
+ PRIMARY KEY (`Id`,`Roomid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
--- Table structure for serverloginlogs
+-- Table structure for ServerLoginLogs
 -- ----------------------------
-DROP TABLE IF EXISTS `serverloginlogs`;
-CREATE TABLE `serverloginlogs` (
-  `ServerName` varchar(255) NOT NULL DEFAULT '',
-  `ServerKey` varchar(255) NOT NULL DEFAULT '',
-  `LoginTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+DROP TABLE IF EXISTS `ServerLoginLogs`;
+CREATE TABLE `ServerLoginLogs` (
+ `ServerName` varchar(255) NOT NULL DEFAULT '',
+ `ServerKey` varchar(255) NOT NULL DEFAULT '',
+ `LoginTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
--- Table structure for stores
+-- Table structure for Stores
 -- ----------------------------
-DROP TABLE IF EXISTS `stores`;
-CREATE TABLE `stores` (
-  `Id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `StoreName` varchar(255) NOT NULL DEFAULT '',
-  `StartTime` datetime DEFAULT NULL,
-  `EndTime` datetime DEFAULT NULL,
-  PRIMARY KEY (`Id`)
+DROP TABLE IF EXISTS `Stores`;
+CREATE TABLE `Stores` (
+ `Id` bigint(20) NOT NULL AUTO_INCREMENT,
+ `StoreName` varchar(255) NOT NULL DEFAULT '',
+ `StartTime` datetime DEFAULT NULL,
+ `EndTime` datetime DEFAULT NULL,
+ PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
--- Table structure for usercharacters
+-- Table structure for UserCharacters
 -- ----------------------------
-DROP TABLE IF EXISTS `usercharacters`;
-CREATE TABLE `usercharacters` (
-  `Id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `CharacterId` bigint(20) NOT NULL DEFAULT '0',
-  `UserId` bigint(20) NOT NULL DEFAULT '0',
-  `Name` varchar(255) NOT NULL DEFAULT '',
-  `FirstName` varchar(255) NOT NULL DEFAULT '',
-  `NickName` varchar(255) NOT NULL DEFAULT '',
-  `PrimaryAttribute` int(1) NOT NULL DEFAULT '0',
-  `InitialATK` double(20,0) NOT NULL DEFAULT '0',
-  `InitialDEF` double(20,0) NOT NULL DEFAULT '0',
-  `InitialHP` double(20,0) NOT NULL DEFAULT '0',
-  `InitialMP` double(20,0) NOT NULL DEFAULT '0',
-  `InitialAGI` double(20,0) NOT NULL DEFAULT '0',
-  `InitialINT` double(20,0) NOT NULL DEFAULT '0',
-  `InitialSTR` double(20,0) NOT NULL DEFAULT '0',
-  `InitialSPD` double(20,0) NOT NULL DEFAULT '0',
-  `InitialHR` double(20,0) NOT NULL DEFAULT '0',
-  `InitialMR` double(20,0) NOT NULL DEFAULT '0',
-  `InSquad` int(1) NOT NULL DEFAULT '0',
-  `TrainingTime` datetime DEFAULT NULL,
-  PRIMARY KEY (`Id`,`CharacterId`,`UserId`)
+DROP TABLE IF EXISTS `UserCharacters`;
+CREATE TABLE `UserCharacters` (
+ `Id` bigint(20) NOT NULL AUTO_INCREMENT,
+ `CharacterId` bigint(20) NOT NULL DEFAULT '0',
+ `UserId` bigint(20) NOT NULL DEFAULT '0',
+ `Name` varchar(255) NOT NULL DEFAULT '',
+ `FirstName` varchar(255) NOT NULL DEFAULT '',
+ `NickName` varchar(255) NOT NULL DEFAULT '',
+ `PrimaryAttribute` int(1) NOT NULL DEFAULT '0',
+ `InitialATK` double(20,0) NOT NULL DEFAULT '0',
+ `InitialDEF` double(20,0) NOT NULL DEFAULT '0',
+ `InitialHP` double(20,0) NOT NULL DEFAULT '0',
+ `InitialMP` double(20,0) NOT NULL DEFAULT '0',
+ `InitialAGI` double(20,0) NOT NULL DEFAULT '0',
+ `InitialINT` double(20,0) NOT NULL DEFAULT '0',
+ `InitialSTR` double(20,0) NOT NULL DEFAULT '0',
+ `InitialSPD` double(20,0) NOT NULL DEFAULT '0',
+ `InitialHR` double(20,0) NOT NULL DEFAULT '0',
+ `InitialMR` double(20,0) NOT NULL DEFAULT '0',
+ `InSquad` int(1) NOT NULL DEFAULT '0',
+ `TrainingTime` datetime DEFAULT NULL,
+ PRIMARY KEY (`Id`,`CharacterId`,`UserId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
--- Table structure for useritems
+-- Table structure for UserItems
 -- ----------------------------
-DROP TABLE IF EXISTS `useritems`;
-CREATE TABLE `useritems` (
-  `Id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `ItemId` bigint(20) NOT NULL DEFAULT '0',
-  `UserId` bigint(20) NOT NULL DEFAULT '0',
-  `CharacterId` bigint(20) NOT NULL DEFAULT '0',
-  `ItemName` varchar(255) NOT NULL DEFAULT '',
-  `IsLock` int(1) NOT NULL DEFAULT '0',
-  `Equipable` int(1) NOT NULL DEFAULT '0',
-  `Unequipable` int(1) NOT NULL DEFAULT '0',
-  `EquipSlotType` int(2) NOT NULL DEFAULT '0',
-  `Key` int(10) NOT NULL DEFAULT '0',
-  `Enable` int(1) NOT NULL DEFAULT '0',
-  `Price` double(20,0) NOT NULL DEFAULT '0',
-  `IsSellable` int(1) NOT NULL DEFAULT '0',
-  `IsTradable` int(1) NOT NULL DEFAULT '0',
-  `NextSellableTime` datetime DEFAULT NULL,
-  `NextTradableTime` datetime DEFAULT NULL,
-  `RemainUseTimes` int(10) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`Id`)
+DROP TABLE IF EXISTS `UserItems`;
+CREATE TABLE `UserItems` (
+ `Id` bigint(20) NOT NULL AUTO_INCREMENT,
+ `ItemId` bigint(20) NOT NULL DEFAULT '0',
+ `UserId` bigint(20) NOT NULL DEFAULT '0',
+ `CharacterId` bigint(20) NOT NULL DEFAULT '0',
+ `ItemName` varchar(255) NOT NULL DEFAULT '',
+ `IsLock` int(1) NOT NULL DEFAULT '0',
+ `Equipable` int(1) NOT NULL DEFAULT '0',
+ `Unequipable` int(1) NOT NULL DEFAULT '0',
+ `EquipSlotType` int(2) NOT NULL DEFAULT '0',
+ `Key` int(10) NOT NULL DEFAULT '0',
+ `Enable` int(1) NOT NULL DEFAULT '0',
+ `Price` double(20,0) NOT NULL DEFAULT '0',
+ `IsSellable` int(1) NOT NULL DEFAULT '0',
+ `IsTradable` int(1) NOT NULL DEFAULT '0',
+ `NextSellableTime` datetime DEFAULT NULL,
+ `NextTradableTime` datetime DEFAULT NULL,
+ `RemainUseTimes` int(10) NOT NULL DEFAULT '0',
+ PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
--- Table structure for userlogs
+-- Table structure for UserLogs
 -- ----------------------------
-DROP TABLE IF EXISTS `userlogs`;
-CREATE TABLE `userlogs` (
-  `Id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `UserId` bigint(20) NOT NULL DEFAULT '0',
-  `Title` varchar(255) NOT NULL DEFAULT '',
-  `Description` varchar(255) NOT NULL DEFAULT '',
-  `Remark` varchar(255) NOT NULL DEFAULT '',
-  `CreateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`Id`)
+DROP TABLE IF EXISTS `UserLogs`;
+CREATE TABLE `UserLogs` (
+ `Id` bigint(20) NOT NULL AUTO_INCREMENT,
+ `UserId` bigint(20) NOT NULL DEFAULT '0',
+ `Title` varchar(255) NOT NULL DEFAULT '',
+ `Description` varchar(255) NOT NULL DEFAULT '',
+ `Remark` varchar(255) NOT NULL DEFAULT '',
+ `CreateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
--- Table structure for users
+-- Table structure for Users
 -- ----------------------------
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-  `Id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `Username` varchar(255) NOT NULL DEFAULT '',
-  `Password` varchar(255) NOT NULL DEFAULT '',
-  `RegTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `LastTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `LastIP` varchar(255) NOT NULL DEFAULT '',
-  `Email` varchar(255) NOT NULL DEFAULT '',
-  `Nickname` varchar(255) NOT NULL DEFAULT '',
-  `IsAdmin` int(1) NOT NULL DEFAULT '0',
-  `IsOperator` int(1) NOT NULL DEFAULT '0',
-  `IsEnable` int(1) NOT NULL DEFAULT '1',
-  `Credits` double(20,0) NOT NULL DEFAULT '0',
-  `Materials` double(20,0) NOT NULL DEFAULT '0',
-  `GameTime` double(20,0) NOT NULL DEFAULT '0',
-  `AutoKey` varchar(255) NOT NULL DEFAULT '',
-  PRIMARY KEY (`Id`,`Username`,`Email`)
+DROP TABLE IF EXISTS `Users`;
+CREATE TABLE `Users` (
+ `Id` bigint(20) NOT NULL AUTO_INCREMENT,
+ `Username` varchar(255) NOT NULL DEFAULT '',
+ `Password` varchar(255) NOT NULL DEFAULT '',
+ `RegTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ `LastTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ `LastIP` varchar(255) NOT NULL DEFAULT '',
+ `Email` varchar(255) NOT NULL DEFAULT '',
+ `Nickname` varchar(255) NOT NULL DEFAULT '',
+ `IsAdmin` int(1) NOT NULL DEFAULT '0',
+ `IsOperator` int(1) NOT NULL DEFAULT '0',
+ `IsEnable` int(1) NOT NULL DEFAULT '1',
+ `Credits` double(20,0) NOT NULL DEFAULT '0',
+ `Materials` double(20,0) NOT NULL DEFAULT '0',
+ `GameTime` double(20,0) NOT NULL DEFAULT '0',
+ `AutoKey` varchar(255) NOT NULL DEFAULT '',
+ PRIMARY KEY (`Id`,`Username`,`Email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
--- Table structure for usersignins
+-- Table structure for UserSignIns
 -- ----------------------------
-DROP TABLE IF EXISTS `usersignins`;
-CREATE TABLE `usersignins` (
-  `UserId` bigint(20) NOT NULL DEFAULT '0',
-  `LastTime` datetime DEFAULT NULL,
-  `Days` int(10) NOT NULL DEFAULT '0',
-  `IsSigned` int(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`UserId`)
+DROP TABLE IF EXISTS `UserSignIns`;
+CREATE TABLE `UserSignIns` (
+ `UserId` bigint(20) NOT NULL DEFAULT '0',
+ `LastTime` datetime DEFAULT NULL,
+ `Days` int(10) NOT NULL DEFAULT '0',
+ `IsSigned` int(1) NOT NULL DEFAULT '0',
+ PRIMARY KEY (`UserId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- Library/SQLScript/fungame_sqlite.sql
+++ Library/SQLScript/fungame_sqlite.sql
@@ -1,6 +1,18 @@
 PRAGMA foreign_keys = OFF;
 
 -- ----------------------------
+-- Table structure for ApiTokens
+-- ----------------------------
+DROP TABLE IF EXISTS "main"."ApiTokens";
+CREATE TABLE ApiTokens (
+ TokenID TEXT NOT NULL DEFAULT '',
+ SecretKey TEXT NOT NULL DEFAULT '',
+ Reference1 TEXT NOT NULL DEFAULT '',
+ Reference2 TEXT NOT NULL DEFAULT '',
+ PRIMARY KEY (TokenID)
+);
+
+-- ----------------------------
 -- Table structure for ForgetVerifyCodes
 -- ----------------------------
 DROP TABLE IF EXISTS "main"."ForgetVerifyCodes";
@@ -9,6 +21,90 @@ CREATE TABLE ForgetVerifyCodes (
  Email TEXT NOT NULL DEFAULT '',
  ForgetVerifyCode TEXT NOT NULL DEFAULT '',
  SendTime DATETIME NOT NULL DEFAULT (DATETIME('now'))
+);
+
+-- ----------------------------
+-- Table structure for GoodItems
+-- ----------------------------
+DROP TABLE IF EXISTS "main"."GoodItems";
+CREATE TABLE GoodItems (
+ Id INTEGER PRIMARY KEY AUTOINCREMENT,
+ GoodId INTEGER NOT NULL DEFAULT 0,
+ ItemId INTEGER NOT NULL DEFAULT 0
+);
+
+-- ----------------------------
+-- Table structure for GoodPrices
+-- ----------------------------
+DROP TABLE IF EXISTS "main"."GoodPrices";
+CREATE TABLE GoodPrices (
+ Id INTEGER PRIMARY KEY AUTOINCREMENT,
+ GoodId INTEGER NOT NULL DEFAULT 0,
+ Currency TEXT NOT NULL DEFAULT '',
+ Price REAL NOT NULL DEFAULT 0
+);
+
+-- ----------------------------
+-- Table structure for Goods
+-- ----------------------------
+DROP TABLE IF EXISTS "main"."Goods";
+CREATE TABLE Goods (
+ Id INTEGER PRIMARY KEY AUTOINCREMENT,
+ Name TEXT NOT NULL DEFAULT '',
+ Description TEXT NOT NULL DEFAULT '',
+ Stock INTEGER NOT NULL DEFAULT 0
+);
+
+-- ----------------------------
+-- Table structure for Inventories
+-- ----------------------------
+DROP TABLE IF EXISTS "main"."Inventories";
+CREATE TABLE Inventories (
+ UserId INTEGER NOT NULL DEFAULT 0,
+ Name TEXT NOT NULL DEFAULT '',
+ Credits REAL NOT NULL DEFAULT 0,
+ Materials REAL NOT NULL DEFAULT 0,
+ MainCharacter INTEGER NOT NULL DEFAULT 0,
+ PRIMARY KEY (UserId)
+);
+
+-- ----------------------------
+-- Table structure for MarketItems
+-- ----------------------------
+DROP TABLE IF EXISTS "main"."MarketItems";
+CREATE TABLE MarketItems (
+ Id INTEGER PRIMARY KEY AUTOINCREMENT,
+ ItemId INTEGER NOT NULL DEFAULT 0,
+ UserId INTEGER NOT NULL DEFAULT 0,
+ Price REAL NOT NULL DEFAULT 0,
+ CreateTime DATETIME NOT NULL DEFAULT (DATETIME('now')),
+ FinishTime DATETIME DEFAULT NULL,
+ Status INTEGER NOT NULL DEFAULT 0,
+ Buyer INTEGER NOT NULL DEFAULT 0
+);
+
+-- ----------------------------
+-- Table structure for OfferItems
+-- ----------------------------
+DROP TABLE IF EXISTS "main"."OfferItems";
+CREATE TABLE OfferItems (
+ Id INTEGER PRIMARY KEY AUTOINCREMENT,
+ OfferId INTEGER NOT NULL DEFAULT 0,
+ UserId INTEGER NOT NULL DEFAULT 0,
+ ItemId INTEGER NOT NULL DEFAULT 0
+);
+
+-- ----------------------------
+-- Table structure for Offers
+-- ----------------------------
+DROP TABLE IF EXISTS "main"."Offers";
+CREATE TABLE Offers (
+ Id INTEGER PRIMARY KEY AUTOINCREMENT,
+ Offeror INTEGER NOT NULL DEFAULT 0,
+ Offeree INTEGER NOT NULL DEFAULT 0,
+ CreateTime DATETIME NOT NULL DEFAULT (DATETIME('now')),
+ FinishTime DATETIME DEFAULT NULL,
+ Status INTEGER NOT NULL DEFAULT 0
 );
 
 -- ----------------------------
@@ -26,19 +122,19 @@ CREATE TABLE RegVerifyCodes (
 -- Table structure for Rooms
 -- ----------------------------
 DROP TABLE IF EXISTS "main"."Rooms";
-CREATE TABLE "Rooms" (
-"Id"  INTEGER PRIMARY KEY AUTOINCREMENT,
-"Roomid"  TEXT NOT NULL DEFAULT '-1',
-"CreateTime"  DATETIME NOT NULL DEFAULT (DATETIME('now')),
-"RoomMaster"  INTEGER NOT NULL DEFAULT 0,
-"RoomType"  INTEGER NOT NULL DEFAULT 0,
-"GameModule"  TEXT NOT NULL DEFAULT '',
-"GameMap"  TEXT NOT NULL DEFAULT '',
-"RoomState"  INTEGER NOT NULL DEFAULT 0,
-"IsRank"  INTEGER NOT NULL DEFAULT 0,
-"HasPass"  INTEGER NOT NULL DEFAULT 0,
-"Password"  TEXT NOT NULL DEFAULT '',
-"MaxUsers" INTEGER NOT NULL DEFAULT 0
+CREATE TABLE Rooms (
+ Id INTEGER PRIMARY KEY AUTOINCREMENT,
+ Roomid TEXT NOT NULL DEFAULT '-1',
+ CreateTime DATETIME NOT NULL DEFAULT (DATETIME('now')),
+ RoomMaster INTEGER NOT NULL DEFAULT 0,
+ RoomType INTEGER NOT NULL DEFAULT 0,
+ GameModule TEXT NOT NULL DEFAULT '',
+ GameMap TEXT NOT NULL DEFAULT '',
+ RoomState INTEGER NOT NULL DEFAULT 0,
+ IsRank INTEGER NOT NULL DEFAULT 0,
+ HasPass INTEGER NOT NULL DEFAULT 0,
+ Password TEXT NOT NULL DEFAULT '',
+ MaxUsers INTEGER NOT NULL DEFAULT 0
 );
 
 -- ----------------------------
@@ -52,15 +148,77 @@ CREATE TABLE ServerLoginLogs (
 );
 
 -- ----------------------------
--- Table structure for ApiTokens
+-- Table structure for Stores
 -- ----------------------------
-DROP TABLE IF EXISTS "main"."ApiTokens";
-CREATE TABLE ApiTokens (
- TokenID TEXT NOT NULL DEFAULT '',
- SecretKey TEXT NOT NULL DEFAULT '',
- Reference1 TEXT NOT NULL DEFAULT '',
- Reference2 TEXT NOT NULL DEFAULT '',
- PRIMARY KEY (TokenID)
+DROP TABLE IF EXISTS "main"."Stores";
+CREATE TABLE Stores (
+ Id INTEGER PRIMARY KEY AUTOINCREMENT,
+ StoreName TEXT NOT NULL DEFAULT '',
+ StartTime DATETIME DEFAULT NULL,
+ EndTime DATETIME DEFAULT NULL
+);
+
+-- ----------------------------
+-- Table structure for UserCharacters
+-- ----------------------------
+DROP TABLE IF EXISTS "main"."UserCharacters";
+CREATE TABLE UserCharacters (
+ Id INTEGER PRIMARY KEY AUTOINCREMENT,
+ CharacterId INTEGER NOT NULL DEFAULT 0,
+ UserId INTEGER NOT NULL DEFAULT 0,
+ Name TEXT NOT NULL DEFAULT '',
+ FirstName TEXT NOT NULL DEFAULT '',
+ NickName TEXT NOT NULL DEFAULT '',
+ PrimaryAttribute INTEGER NOT NULL DEFAULT 0,
+ InitialATK REAL NOT NULL DEFAULT 0,
+ InitialDEF REAL NOT NULL DEFAULT 0,
+ InitialHP REAL NOT NULL DEFAULT 0,
+ InitialMP REAL NOT NULL DEFAULT 0,
+ InitialAGI REAL NOT NULL DEFAULT 0,
+ InitialINT REAL NOT NULL DEFAULT 0,
+ InitialSTR REAL NOT NULL DEFAULT 0,
+ InitialSPD REAL NOT NULL DEFAULT 0,
+ InitialHR REAL NOT NULL DEFAULT 0,
+ InitialMR REAL NOT NULL DEFAULT 0,
+ InSquad INTEGER NOT NULL DEFAULT 0,
+ TrainingTime DATETIME DEFAULT NULL
+);
+
+-- ----------------------------
+-- Table structure for UserItems
+-- ----------------------------
+DROP TABLE IF EXISTS "main"."UserItems";
+CREATE TABLE UserItems (
+ Id INTEGER PRIMARY KEY AUTOINCREMENT,
+ ItemId INTEGER NOT NULL DEFAULT 0,
+ UserId INTEGER NOT NULL DEFAULT 0,
+ CharacterId INTEGER NOT NULL DEFAULT 0,
+ ItemName TEXT NOT NULL DEFAULT '',
+ IsLock INTEGER NOT NULL DEFAULT 0,
+ Equipable INTEGER NOT NULL DEFAULT 0,
+ Unequipable INTEGER NOT NULL DEFAULT 0,
+ EquipSlotType INTEGER NOT NULL DEFAULT 0,
+ Key INTEGER NOT NULL DEFAULT 0,
+ Enable INTEGER NOT NULL DEFAULT 0,
+ Price REAL NOT NULL DEFAULT 0,
+ IsSellable INTEGER NOT NULL DEFAULT 0,
+ IsTradable INTEGER NOT NULL DEFAULT 0,
+ NextSellableTime DATETIME DEFAULT NULL,
+ NextTradableTime DATETIME DEFAULT NULL,
+ RemainUseTimes INTEGER NOT NULL DEFAULT 0
+);
+
+-- ----------------------------
+-- Table structure for UserLogs
+-- ----------------------------
+DROP TABLE IF EXISTS "main"."UserLogs";
+CREATE TABLE UserLogs (
+ Id INTEGER PRIMARY KEY AUTOINCREMENT,
+ UserId INTEGER NOT NULL DEFAULT 0,
+ Title TEXT NOT NULL DEFAULT '',
+ Description TEXT NOT NULL DEFAULT '',
+ Remark TEXT NOT NULL DEFAULT '',
+ CreateTime DATETIME NOT NULL DEFAULT (DATETIME('now'))
 );
 
 -- ----------------------------
@@ -68,9 +226,9 @@ CREATE TABLE ApiTokens (
 -- ----------------------------
 DROP TABLE IF EXISTS "main"."Users";
 CREATE TABLE Users (
- UID INTEGER PRIMARY KEY AUTOINCREMENT,
- Username TEXT NOT NULL,
- Password TEXT NOT NULL,
+ Id INTEGER PRIMARY KEY AUTOINCREMENT,
+ Username TEXT NOT NULL DEFAULT '',
+ Password TEXT NOT NULL DEFAULT '',
  RegTime DATETIME NOT NULL DEFAULT (DATETIME('now')),
  LastTime DATETIME NOT NULL DEFAULT (DATETIME('now')),
  LastIP TEXT NOT NULL DEFAULT '',
@@ -83,4 +241,16 @@ CREATE TABLE Users (
  Materials REAL NOT NULL DEFAULT 0,
  GameTime REAL NOT NULL DEFAULT 0,
  AutoKey TEXT NOT NULL DEFAULT ''
+);
+
+-- ----------------------------
+-- Table structure for UserSignIns
+-- ----------------------------
+DROP TABLE IF EXISTS "main"."UserSignIns";
+CREATE TABLE UserSignIns (
+ UserId INTEGER NOT NULL DEFAULT 0,
+ LastTime DATETIME DEFAULT NULL,
+ Days INTEGER NOT NULL DEFAULT 0,
+ IsSigned INTEGER NOT NULL DEFAULT 0,
+ PRIMARY KEY (UserId)
 );
