91f50c29c2f8c2fcd630c08eb0b0c83af9210c70
milimoe <mili@wrss.org>
2025-03-29 16:22:16 +0800

    添加  NOTICE 文件

--- Library/Constant/FunGameInfo.cs
+++ Library/Constant/FunGameInfo.cs
@@ -33,7 +33,8 @@
 
         public const int FunGame_Version_Major = 1;
         public const int FunGame_Version_Minor = 0;
-        public const string FunGame_VersionPatch = ".0-rc1";
+        public const string FunGame_VersionPatch = ".0-rc.1";
+        public const string FunGame_Version_Build = "";
 
         public const string FunGameCoreTitle = @"  _____ _   _ _   _  ____    _    __  __ _____    ____ ___  ____  _____ 
  |  ___| | | | \ | |/ ___|  / \  |  \/  | ____|  / ___/ _ \|  _ \| ____|
